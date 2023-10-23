fa880d1a2a9902a4a7b729c190f9e15459ab2089
stevienn <stevenhartanto0603@gmail.com>
2023-10-23 22:15:49 +0700

    login commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ URL::asset('css/login.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login-box">
+        <form action="">
+            <h2>Login</h2>
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="button" value="Log In">
+        </form>
+        
+    </div>
+</body>
+</html>
