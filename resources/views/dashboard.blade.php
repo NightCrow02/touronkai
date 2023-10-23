f8b8e015cd9a106c9876c4d9790adee4841cc655
Calvin Nelson <nelsoncalvin10@gmail.com>
2023-10-23 22:19:34 +0700

    dashboard update

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+</head>
+<body>
+    <!-- navigation bar -->
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 offset-sm-3 col-md-6 offset-md-3">
+        
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/todo/active">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/todo/done">Done</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/todo/deleted">Deleted</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+  </div>
+  <!-- navigation bar ends here -->
+</body>
+</html>
