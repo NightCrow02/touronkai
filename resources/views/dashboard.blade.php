106145f1170f892bc43b4ac3f2a47f1aaee4e091
stevienn <stevenhartanto0603@gmail.com>
2023-10-24 00:04:28 +0700

    dasbod commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,11 +10,31 @@
 </head>
 <body>
     <!-- navigation bar -->
+    {{-- <div class="nav-box1">
+        <h2>Dashboard</h2>
+    </div>
+    <div class="nav-box2">
+        <h1>Contacts</h1>
+    </div> --}}
+    <!-- Nav tabs -->
+
     <div class="box">
         <div class="header-box">
-            <h1>Mails</h1>
+             <h1>Mails</h1>
         </div>
     </div>
+    <div class="groups-box">
+        <div class="header-box2">
+            <h1>Groups</h1>
+        </div>
+    </div>
+    <div class="contacts-box">
+        <div class="header-box3">
+            <h1>Contacts</h1>
+        </div>
+    </div>
+    
+
   <!-- navigation bar ends here -->
 </body>
 </html>
