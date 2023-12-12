878d6f7918c1f4dde6fa924f9709fd19bce7c5e2
Calvin <nelsoncalvin10@gmail.com>
2023-12-12 15:26:14 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('dashboard');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
